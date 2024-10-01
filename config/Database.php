a74d01721df7102f915920a9c1277684a68492b4
Abdel780 <abdelkarim27200@gmail.com>
2024-10-01 14:27:48 +0200

    creation base de données

---
+++ config/Database.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Config;
+use PDO;
+use Exception;
+class DataBase
+{
+    static function getConnection()
+    {
+        try {
+            $pdo = new PDO('mysql:host=localhost;dbname=todolist;charset=utf8', 'root');
+        } catch (Exception $e) {
+            die('Erreur :' . $e->getMessage());
+        }
+        return $pdo;
+    }
+}
