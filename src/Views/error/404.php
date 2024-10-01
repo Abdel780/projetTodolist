ed0bae5d99be9ee2169336fef6ea190293d962b4
Abdel780 <abdelkarim27200@gmail.com>
2024-10-01 14:02:25 +0200

    création nav et footer

--- src/Views/error/404.php
+++ src/Views/error/404.php
@@ -1,5 +1,12 @@
+<?php 
+    require_once(__DIR__. '/../partials/head.php')
+?>
 
     <div class="erreur">
         <h1>ERREUR 404 </h1>
         <a class="btn btn-primary" href="/" role="button">Accueil</a>
     </div>
+
+<?php 
+    require_once(__DIR__. '/../partials/footer.php')
+?>
